1404398c9568a0e9d1e94480987ffc9b59be36b9
sonhmai <>
2023-09-18 17:31:29 +0700

    Can parse command with db file and sql str

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rust-sqlite
 Rewriting SQLite in Rust for Learning and Fun
+
+```
+cargo run -- sql sample.db "select name from table"
+```
--- src/bin/rsql.rs
+++ src/bin/rsql.rs
@@ -1,4 +1,22 @@
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    println!("Hello, world!");
+
+    let matches = App::new("rust-sqlite")
+        .subcommand(
+            SubCommand::with_name("sql")
+                .about("Execute sql query against a db file")
+                .arg(Arg::with_name("db").required(true))
+                .arg(Arg::with_name("sql").help("SQL string to execute").required(true))
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("sql", Some(_matches)) => {
+            let db = _matches.value_of("db").unwrap();
+            let sqlstr = _matches.value_of("sql").unwrap();
+            println!("Executing {sqlstr} against {db}")
+        }
+        _ => unreachable!()
+    }
 }
