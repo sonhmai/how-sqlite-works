f3e942a63d202b2b1889bf0e4b0590d77c081cad
sonhmai <>
2023-09-22 15:53:18 +0700

    Can output all columns from dummy apples scan

--- src/physical/exec_apples_scan.rs
+++ src/physical/exec_apples_scan.rs
@@ -1,3 +1,4 @@
+use crate::model::column::ColumnValue;
 use crate::model::data_record::DataRecord;
 use crate::physical::exec::Exec;
 
@@ -8,6 +9,39 @@ pub struct ExecApplesScan {}
 
 impl Exec for ExecApplesScan {
     fn execute(&self) -> Vec<DataRecord> {
-        vec![]
+        vec![
+            DataRecord {
+                values: vec![
+                    ColumnValue::Text(b"Granny Smith"),
+                    ColumnValue::Text(b"Light Green"),
+                ],
+                num_header_bytes: 0,
+                rowid: Some(1),
+            },
+            DataRecord {
+                values: vec![
+                    ColumnValue::Text(b"Fuji"),
+                    ColumnValue::Text(b"Red"),
+                ],
+                num_header_bytes: 0,
+                rowid: Some(2),
+            },
+            DataRecord {
+                values: vec![
+                    ColumnValue::Text(b"Honeycrisp"),
+                    ColumnValue::Text(b"Blush Red"),
+                ],
+                num_header_bytes: 0,
+                rowid: Some(3),
+            },
+            DataRecord {
+                values: vec![
+                    ColumnValue::Text(b"Golden Delicious"),
+                    ColumnValue::Text(b"Yellow"),
+                ],
+                num_header_bytes: 0,
+                rowid: Some(4),
+            },
+        ]
     }
 }
