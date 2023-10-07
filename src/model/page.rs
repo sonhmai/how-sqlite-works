bb088f864f691b5d5dc00122ca0c7e94abc93c61
sonhmai <14060682+sonhmai@users.noreply.github.com>
2023-10-07 09:53:02 +0700

    Add FirstPage

--- src/model/page.rs
+++ src/model/page.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use crate::model::db_header::DbHeader;
 
 use crate::model::page_header::{PageHeader, PageType};
 use crate::model::page_id::PageId;
@@ -13,13 +14,6 @@ use crate::model::page_id::PageId;
 pub struct Page {
     pub page_header: PageHeader,
     pub page_id: PageId,
-}
-
-/// The first page is special because it contains
-/// the sqlite_schema table.
-#[derive(Debug)]
-pub struct FirstPageSchema {
-    pub page_header: PageHeader,
 }
 
 impl Page {
@@ -48,6 +42,37 @@ impl Page {
     }
 }
 
+
+/// The first page is special because it contains
+/// the sqlite_schema table.
+#[derive(Debug)]
+pub struct FirstPage {
+    pub page_header: PageHeader,
+    pub page_id: PageId,
+}
+
+impl FirstPage {
+
+    const PAGE_NUMBER: u32 = 1;
+
+    pub fn parse(db: &[u8]) -> Result<Self> {
+        // first page data starts after the DBHeader 100 bytes
+        // https://www.sqlite.org/fileformat.html#b_tree_pages
+        Ok(FirstPage {
+            page_header: PageHeader::parse(&db[DbHeader::SIZE..])?,
+            page_id: PageId { page_number: Self::PAGE_NUMBER },
+        })
+    }
+
+    fn get_id(&self) -> PageId {
+        self.page_id
+    }
+
+    fn get_page_number(&self) -> u32 {
+        self.page_id.page_number
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
@@ -55,18 +80,32 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn test_parse_table_leaf_page() {
+    fn db_bytes() -> Vec<u8> {
         // CARGO_MANIFEST_DIR is project root /../rust-sqlite
         let db_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
             .join("tests/resources/sample.db");
         let data = fs::read(db_path).unwrap();
-        let db_slice = data.as_slice();
+        data
+    }
+
+    #[test]
+    fn test_parse_table_leaf_page() {
+        let db = db_bytes();
         let first_data_page_number = 2;
-        let page = Page::parse(first_data_page_number, 4096, db_slice).unwrap();
+        let page = Page::parse(first_data_page_number, 4096, db.as_slice()).unwrap();
 
         assert_eq!(page.get_page_number(), 2);
         assert_eq!(page.page_header.page_type, PageType::LeafTable);
         assert_eq!(page.page_header.number_of_cells, 4);
+    }
+
+    #[test]
+    fn test_parse_first_page() {
+        let db = db_bytes();
+        let first_page = FirstPage::parse(db.as_slice()).unwrap();
+
+        assert_eq!(first_page.get_page_number(), 1);
+        assert_eq!(first_page.page_header.page_type, PageType::LeafTable);
+        assert_eq!(first_page.page_header.number_of_cells, 3);
     }
 }
