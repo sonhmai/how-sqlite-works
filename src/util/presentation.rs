eb5ff09994395cd362a3b5c749d9d93d753f88c4
sonhmai <>
2023-10-20 12:48:50 +0700

    Refactor show sqlite to util/presentation

---
+++ src/util/presentation.rs
@@ -0,0 +1,15 @@
+use crate::model::data_record::DataRecord;
+
+/// show CLI output in SQLite format
+pub fn sqlite_show(records: & Vec<DataRecord>) {
+    for record in records {
+        let formatted_values: Vec<String> = record
+            .values
+            .iter()
+            .map(|value| value.to_string())
+            .collect();
+
+        let output = formatted_values.join("|");
+        println!("{}", output);
+    }
+}
