5a90aa4cff15fcd1eafafb7880d60eee4f1e94cb
sonhmai <>
2023-11-16 12:55:17 +0700

    Add doc trait Exec

--- src/physical/plan/exec.rs
+++ src/physical/plan/exec.rs
@@ -1,7 +1,11 @@
 use crate::model::data_record::DataRecord;
 use std::fmt::Debug;
 
+/// Represent node in Physical Plan Tree
 pub trait Exec: Debug {
     // TODO use Iterator?
     fn execute(&mut self) -> Vec<DataRecord>;
+
+    // Get the schema for this Physical Plan
+    // fn schema(&self) -> SchemaRef; // ??
 }
