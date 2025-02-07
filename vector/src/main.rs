64b6980d6b8bae916f3a3cee79ddd8686eaac204
sonhmai <>
2025-02-07 11:09:27 +0700

    add vector

--- vector/src/main.rs
+++ vector/src/main.rs
@@ -1,8 +1,9 @@
-use sqlite_vec::sqlite3_vec_init;
 use rusqlite::{ffi::sqlite3_auto_extension, Connection, Result};
+use sqlite_vec::sqlite3_vec_init;
 use zerocopy::IntoBytes;
 
-fn main()-> Result<()> {
+fn main() -> Result<()> {
+    // https://github.com/asg017/sqlite-vec/tree/main/examples/simple-rust
     unsafe {
         sqlite3_auto_extension(Some(std::mem::transmute(sqlite3_vec_init as *const ())));
     }
@@ -17,5 +18,39 @@ fn main()-> Result<()> {
     )?;
 
     println!("vec_version={vec_version}, embedding={embedding}");
+    let items: Vec<(usize, Vec<f32>)> = vec![
+        (1, vec![0.1, 0.1, 0.1, 0.1]),
+        (2, vec![0.2, 0.2, 0.2, 0.2]),
+        (3, vec![0.3, 0.3, 0.3, 0.3]),
+        (4, vec![0.4, 0.4, 0.4, 0.4]),
+        (5, vec![0.5, 0.5, 0.5, 0.5]),
+    ];
+
+    db.execute(
+        "create virtual table vec_items using vec0(embedding float[4])",
+        [],
+    )?;
+    let mut stmt = db.prepare("insert into vec_items(rowid, embedding) values (?, ?)")?;
+    for item in items {
+        stmt.execute(rusqlite::params![item.0, item.1.as_bytes()])?;
+    }
+    let query: Vec<f32> = vec![0.3, 0.3, 0.3, 0.3];
+    let result: Vec<(i64, f64)> = db
+        .prepare(
+            r"
+          SELECT
+            rowid,
+            distance,
+            embedding
+          FROM vec_items
+          WHERE embedding MATCH ?1
+          ORDER BY distance
+          LIMIT 10 -- limit is needed in knn search
+        ",
+        )?
+        .query_map([query.as_bytes()], |r| Ok((r.get(0)?, r.get(1)?)))?
+        .collect::<Result<Vec<_>, _>>()?;
+    println!("{:?}", result);
+
     Ok(())
 }
